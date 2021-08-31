a23ea5893665a008934d37939f160f59b053a73d
Shardul Bhardwaj <shardulb19@gmail.com>
2021-08-31 01:03:59 -0400

    added function to find free games

--- getCurrentFreeGames.ts
+++ getCurrentFreeGames.ts
@@ -1,8 +1,7 @@
 // https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions
 import axios from 'axios';
 
-
-export async function getCurrentFreeGames(){
+export const getCurrentFreeGames = async () => {
     const res = await axios.get('https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions');
     const games: any[] = res.data.data.Catalog.searchStore.elements;
     return games.filter(game => game.price.totalPrice.discountPrice === 0 && game.price.totalPrice.originalPrice > 0 );
