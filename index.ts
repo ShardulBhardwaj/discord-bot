de5efe05fe0093ba344294435c2a302bbb273e78
Shardul Bhardwaj <shardulb19@gmail.com>
2021-09-08 16:03:57 -0400

    added funct

--- index.ts
+++ index.ts
@@ -1,19 +1,31 @@
-const dotenv = require('dotenv');
+import { getCurrentFreeGames } from "./getCurrentFreeGames"
+import dotenv from 'dotenv';
 dotenv.config();
 
-import { Client, Intents, TextChannel } from 'discord.js';
+import { Client, Intents, MessageEmbed, TextChannel } from 'discord.js';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-function run() {
+async function run() {
   console.log(`Logged in as ${client.user?.tag}!`);
+  // get channel
   const channel = client.channels.cache.get(process.env.CHANNEL_ID!) as TextChannel;
 
-  channel.send('https://www.epicgames.com/store/en-US/');
+  const freeGames = await getCurrentFreeGames();
+
+  const embeds = freeGames.map(game => new MessageEmbed()
+                                    .setColor('#0099ff')
+                                    .setTitle(`${game.title}`)
+                                    .setURL(`https://www.epicgames.com/store/en-US/p/${game.urlSlug}`)
+                                    .setAuthor(`${game.title}`,game.keyImages.find(image => image.type === 'Thumbnail').url,`https://www.epicgames.com/store/en-US/p/${game.urlSlug}`)
+                                    .setDescription(`${game.description}`)
+                                    .setThumbnail(game.keyImages.find((image: { type: string; }) => image.type === 'Thumbnail').url)
+                                    .setImage(game.keyImages.find(image => image.type === 'Thumbnail').url)
+                                    .setTimestamp()
+                              )
+
+  channel.send({ embeds: embeds });
 }
 
-
 client.on('ready', run);
-
-
 
 client.login(process.env.TOKEN);
